972771b4092bbbef4cd9899e2c5a6b230fe4e0b8
折原七瀬 <n-orihara@cmind-co.jp>
2020-08-18 01:06:55 +0900

    【課題提出】第3章 - Laravel実践 - 第10部 - 投稿データを保存しよう (復習)

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでWork Modelが扱えるようになる
+use App\Work;
+
 class WorkController extends Controller
 {
     public function add()
@@ -14,6 +17,29 @@ class WorkController extends Controller
 
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, Work::$rules);
+
+        $work = new Work;
+        $form = $request->all();
+
+        // フォームから資料ファイルが送信されてきたら、保存して、$work->file に資料ファイルのパスを保存する
+        if (isset($form['file'])) {
+            $path = $request->file('file')->store('public/file');
+            $work->file = basename($path);
+        } else {
+            $work->file = null;
+        }
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたfileを削除する
+        unset($form['file']);
+
+        // データベースに保存する
+        $work->fill($form);
+        $work->save();
+
         return redirect('admin/work/create');
     }
 
