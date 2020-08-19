b47a8bef4a5dbddb477e6f3e79f7bc2205ce383c
折原七瀬 <n-orihara@cmind-co.jp>
2020-08-20 01:50:52 +0900

    【課題提出】第3章 - Laravel実践 - 第12部 - 投稿したニュースを更新/削除しよう (復習)

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -21,6 +21,7 @@ class WorkController extends Controller
         $this->validate($request, Work::$rules);
 
         $work = new Work;
+        // 送信されてきたフォームデータを格納する
         $form = $request->all();
 
         // フォームから資料ファイルが送信されてきたら、保存して、$work->file に資料ファイルのパスを保存する
@@ -40,16 +41,64 @@ class WorkController extends Controller
         $work->fill($form);
         $work->save();
 
-        return redirect('admin/work/create');
+        return redirect('admin/work/');
     }
 
-    public function edit()
+    public function index(Request $request)
     {
-        return view('admin.work.edit');
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Work::where('name', $cond_name)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Work::all();
+        }
+        return view('admin.work.index',['posts'=>$posts, 'cond_name'=>$cond_name]);
     }
 
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/work/edit');
+        // Work Modelからデータを取得する
+        $work = Work::find($request->id);
+        if (empty($work)) {
+          abort(404);
+        }
+        return view('admin.work.edit', ['form' => $work]);
     }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Work::$rules);
+        // Work Modelからデータを取得する
+        $work = Work::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $form = $request->all();
+        // フォームから資料ファイルが送信されてきたら、保存して、$work->file に資料ファイルのパスを保存する
+        if (isset($form['file'])) {
+          $path = $request->file('file')->store('public/file');
+          $work->file = basename($path);
+          unset($form['file']);
+          //参考資料を削除のチェックボックスにチェックがついたら処理される
+        } elseif (0 == strcmp($request->remove, 'true')) {
+          $work->file = null;
+        }
+        unset($form['_token']);
+        unset($form['remove']);
+  
+        // 該当するデータを上書きして保存する(※省略形)
+        $work->fill($form)->save();
+  
+        return redirect('admin/work');
+    }
+
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $news = Work::find($request->id);
+        // 削除する
+        $news->delete();
+        return redirect('admin/work/');
+    }  
 }
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('work/create', 'Admin\WorkController@add');
     Route::post('work/create', 'Admin\WorkController@create');
+    Route::get('work', 'Admin\WorkController@index');
     Route::get('work/edit', 'Admin\WorkController@edit');
     Route::post('work/edit', 'Admin\WorkController@update');
+    Route::get('work/delete', 'Admin\WorkController@delete');
 });
 
 Auth::routes();
